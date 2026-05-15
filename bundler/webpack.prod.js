2f568aa00a4c5182c35bd7b1f98920354a8a6134
tianBreznik <tian.breza@gmail.com>
2026-05-16 00:29:34 +0200

    actions

--- bundler/webpack.prod.js
+++ bundler/webpack.prod.js
@@ -2,10 +2,21 @@ const { merge } = require('webpack-merge')
 const commonConfiguration = require('./webpack.common.js')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
+function productionPublicPath() {
+
+	const p = process.env.PUBLIC_PATH
+	if ( ! p ) return '/'
+	return p.endsWith( '/' ) ? p : `${p}/`
+
+}
+
 module.exports = merge(
     commonConfiguration,
     {
         mode: 'production',
+        output: {
+            publicPath: productionPublicPath(),
+        },
         plugins:
         [
             new CleanWebpackPlugin()
